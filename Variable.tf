f173e17323d0171cc0a5cfd6e7dfcab3db26a67b
Alekhya-kandala <alekhyagoud564@gmail.com>
2026-08-16 14:04:11 +0530

    variable.tf

---
+++ Variable.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  type=string
+  default = "ami-01a00762f46d584a1"
+}
+variable "instance_type" {
+  type = string 
+  default = "t3.micro"
+}
+variable "name_1" {
+  default = "MY OWN Creation"
+}
